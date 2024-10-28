2ca3b6d729bef62f7d054aeef887955a7bf8de46
Ubuntu <ubuntu@ip-172-31-54-248.ec2.internal>
2024-10-28 05:40:49 +0000

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,12 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(uid: params[:user][:uid], password: params[:user][:password])
-    @user.save
-    redirect_to users_path
+    @user = User.new(uid: params[:user][:uid], password: params[:user][:password], password_confirmation: params[:user][:password_confirmation])
+    if @user.save
+      redirect_to users_path
+    else
+      render :new # Show the form again with error messages
+    end
   end
   
   def destroy
